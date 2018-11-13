e12c6980b9b4eace7a8861523d3faa274275d426
Alimul Hakim <alim.blacksas@gmail.com>
2018-11-13 11:44:03 +0700

    google maps

--- map/config/app.php
+++ map/config/app.php
@@ -159,7 +159,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        FarhanWazir\GoogleMaps\GMapsServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -221,6 +221,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'GMaps' => FarhanWazir\GoogleMaps\Facades\GMapsFacade::class,
 
     ],
 
